696c105ef3ab25981916ecb3a4b08c607b7e7588
venwhere.github.io <venwhere.github.io@163.com>
2018-08-27 11:12:16 +0800

    update master branch

--- src/jsfunctions.js
+++ src/jsfunctions.js
@@ -15,3 +15,35 @@ function getObjectURL(file) {
 	}
 	return url;
 }
+
+/**
+ * 判空函数
+ * @param  {[type]}  val [description] 字符串入参
+ * @return {Boolean}     [description]
+ */
+function isBlank(val){
+	return ((undefined == val) || (null == val) || ("undefined" == val) || ("" == val))
+}
+
+/**
+ * 转为unicode编码函数
+ * @param  {[type]} str [description] 字符串入参
+ * @return {[type]}     [description]
+ */
+function encodeUnicode(str){
+	var res = [];
+	for(var i = 0; i < str.length; i++){
+		res[i] = ("00" + str.charCodeAt(i).toString(16).slice(-4));
+	}
+	return "\\u" + res.join("\\u");
+}
+
+/**
+ * unicode解码函数
+ * @param  {[type]} str [description] 需要解码的字符串
+ * @return {[type]}     [description]
+ */
+function decodeUnicode(str){
+	str = str.replace(/\\/g,"%");
+	return unescape(str);
+}
